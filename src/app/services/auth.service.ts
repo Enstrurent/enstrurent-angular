3938bd9ac056d4572601f42b1fb35f27c9dabcc3
Seda Şimşek <sedasimsekk27@gmail.com>
2021-05-31 20:36:05 +0300

    Sing_up method + roles added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { ClientRegisterModel } from '../models/clientRegisterModel';
 import { LoginModel } from '../models/loginModel';
 import { RegisterModel } from '../models/registerModel';
 import { SingleResponseModel } from '../models/singleResponseModel';
@@ -13,14 +15,17 @@ import { LocalStorageService } from './local-storage.service';
   providedIn: 'root'
 })
 export class AuthService {
-
-  
+  role:any;
+  roles: any[] = [];
+  token: any;
+  isLoggedIn: boolean = false;
   baseURL = environment.baseURL + 'auth/';
 
   constructor(
     private httpClient:HttpClient,
     private router:Router,
-    private localStorageService:LocalStorageService
+    private localStorageService:LocalStorageService,
+    private jwtHelperService:JwtHelperService
     ) { }
 
 
@@ -28,6 +33,10 @@ export class AuthService {
       return this.httpClient.post<SingleResponseModel<TokenModel>>(this.baseURL+"login",loginModel)
     }
   
+    sing_up(clientRegisterModel:ClientRegisterModel){
+      return this.httpClient.post<SingleResponseModel<TokenModel>>(this.baseURL+"sing_up",clientRegisterModel)
+    }
+
     isAuthenticated(){
       if(localStorage.getItem("token")){
         return true;
