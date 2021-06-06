2916d458867ad113884aaf9ff178e111beca5bd6
Seda Şimşek <sedasimsekk27@gmail.com>
2021-06-06 03:00:43 +0300

    Product methods added to product service.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  
+  baseURL = environment.baseURL
+  constructor(private httpClient:HttpClient) { }
+
+  getProduct(product:Product):Observable<SingleResponseModel<Product>>{
+    let newPath = this.baseURL + 'product'
+    return this.httpClient.get<SingleResponseModel<Product>>(newPath)
+  }
+
+  getProducts():Observable<ListResponseModel<Product>>{
+    let newPath = this.baseURL + 'products'
+    return this.httpClient.get<ListResponseModel<Product>>(newPath)
+  }
+
+  addProduct(product:Product):Observable<ResponseModel>{
+    let newPath = this.baseURL + 'products'
+    return this.httpClient.post<ResponseModel>(newPath,product)
+  }
+
+  updateProduct(product:Product):Observable<ResponseModel>{
+    let newPath = this.baseURL + 'products'
+    return this.httpClient.post<ResponseModel>(newPath,product)
+  }
+
 }
