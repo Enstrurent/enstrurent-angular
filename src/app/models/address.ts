cd9d36df0ca30cf1b85cef2fc2c55e008d0b7a98
Seda Şimşek <sedasimsekk27@gmail.com>
2021-06-13 03:22:56 +0300

    Address model added.

---
+++ src/app/models/address.ts
@@ -0,0 +1,11 @@
+export interface Address{
+	id:number
+    title:string 
+	city:string 
+	state:string 
+	zipcode:string
+	street:string
+	buildingNo:string 
+	addressLine:string 
+	description:string
+}
