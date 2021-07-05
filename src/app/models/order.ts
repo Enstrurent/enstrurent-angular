cec193c3706dd5a464d9551c994ccd81106446bf
Seda Şimşek <sedasimsekk27@gmail.com>
2021-07-05 19:17:44 +0300

    Order model added.

--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -1,9 +1,18 @@
-export interface OrderBase{
-	id:number
-	productID:string 
-	clientID:string 
-	addressID:string 
-	deliveryType:string 
-	paymentMethod:string 
-	orderStatus:string
+import { Time } from "@angular/common";
+import { Base } from "./base";
+
+export interface OrderBase extends Base{
+	address_id:number
+	client_id:number
+	product_id:number
+	delivery_type:string 
+	order_status:string
+	payment_method:string
+	price:number
+	initial_image_names:string[] 
+	final_image_names:string
+	number_of_days_for_rent:number      
+	deposit_price:number
+	renting_price:number
+	rented_days_range:string[][]
 }
