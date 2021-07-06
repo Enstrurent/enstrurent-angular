aada3bb446b9ea034cfd4f9d86ac0932752c1f86
Seda Şimşek <sedasimsekk27@gmail.com>
2021-07-06 17:13:55 +0300

    Order type added.

--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -5,6 +5,7 @@ export interface OrderBase extends Base{
 	address_id:number
 	client_id:number
 	product_id:number
+	renter_id:number
 	delivery_type:string 
 	order_status:string
 	payment_method:string
