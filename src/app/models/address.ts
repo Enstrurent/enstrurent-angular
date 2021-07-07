af28f0c8f1877ab8ebf0b8eabcf78a328dd0cdd5
Seda Şimşek <sedasimsekk27@gmail.com>
2021-07-08 01:59:56 +0300

    Address model updated.

--- src/app/models/address.ts
+++ src/app/models/address.ts
@@ -1,11 +1,12 @@
-export interface Address{
-	id:number
+import { Base } from "./base";
+
+export interface Address extends Base{
     title:string 
 	city:string 
 	state:string 
 	zipcode:string
 	street:string
-	buildingNo:string 
-	addressLine:string 
+	building_no:string 
+	address_line:string 
 	description:string
 }
