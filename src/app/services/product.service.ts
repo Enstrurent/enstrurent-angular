3b72cae717b8697d51917d582eab9d337f4ae6a8
Seda Şimşek <sedasimsekk27@gmail.com>
2021-07-08 18:51:12 +0300

    New method added.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,38 +1,34 @@
 import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { config, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Base } from '../models/base';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ProductDetail } from '../models/productDetail';
 import { ResponseModel } from '../models/responseModel';
-import { SingleResponseModel } from '../models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  
-  baseURL = environment.baseURL
+  currentProduct:Product
+  baseURL = environment.baseURL + 'products/'
   constructor(private httpClient:HttpClient) { }
 
-  getProduct(id:number):Observable<ListResponseModel<Product>>{
-    let newPath = this.baseURL + 'products/' + id
-    return this.httpClient.get<ListResponseModel<Product>>(newPath)
+  
+  getOneProduct(id:number){
+    return this.httpClient.get(this.baseURL +id);
   }
 
   getAllProducts():Observable<ListResponseModel<Product>>{
-    let newPath = this.baseURL + 'products/'
-    return this.httpClient.get<ListResponseModel<Product>>(newPath)
-  }
-
-  getRenterProducts(renterId:number):Observable<ListResponseModel<Product>>{
-    let newPath = this.baseURL + 'products/by_renter'
+    let newPath = this.baseURL
     return this.httpClient.get<ListResponseModel<Product>>(newPath)
   }
 
   add(product:Product):Observable<ResponseModel>{
-    let newPath = this.baseURL + 'products'
+    let newPath = this.baseURL + 'products' 
     return this.httpClient.post<ResponseModel>(newPath,product)
   }
 
@@ -44,5 +40,13 @@ export class ProductService {
   delete(product:Product):Observable<ResponseModel>{
     let newPath = this.baseURL + 'products/'
     return this.httpClient.delete<ResponseModel>(newPath)
+  }
+
+  setCurrentProduct(product:Product) {
+    this.currentProduct = product;
+  }
+  
+  getCurrentProduct() {
+    return this.currentProduct;
   }
 }
